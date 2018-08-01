10dd92da030e82ede51b8d13df7f19ffc48bebb4
AderSantos <adersantos@yahoo.com.br>
2018-08-01 11:24:15 -0300

    Implementado notas

---
+++ XPMetronome/XPMetronome.Console/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading;
+
+namespace XPMetronome.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configMetronomo = new ConfiguracaoMetronomo();
+
+            System.Console.WriteLine("Tipo Compasso: ");
+            configMetronomo.TipoCompasso = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("Velocidade: ");
+            configMetronomo.Velocidade = int.Parse(System.Console.ReadLine());
+
+
+            Note[] metronomo = { new Note(Tone.C,Duration.QUARTER),
+                                 new Note(Tone.D,Duration.QUARTER),
+                                 new Note(Tone.E,Duration.QUARTER),
+                                 new Note(Tone.F,Duration.QUARTER),
+                                 new Note(Tone.G,Duration.QUARTER),
+                                 new Note(Tone.A2,Duration.QUARTER),
+                                 new Note(Tone.B2,Duration.QUARTER),
+                                 new Note(Tone.C2,Duration.QUARTER)};
+            Play(metronomo);
+        }
+
+        private static void Play(Note[] metronomo)
+        {
+            foreach (var nota in metronomo)
+            {
+                if (nota.NoteTone == Tone.REST)
+                    Thread.Sleep((int)nota.NoteDuration);
+                else
+                    System.Console.Beep((int)nota.NoteTone, (int)nota.NoteDuration);
+            }
+        }
+
+        private static void ExecutarBeep(int tipoCompasso, int velocidade)
+        {
+            int frequencia = 0;
+            int duracao = 0;
+            var compasso = int.Parse(tipoCompasso.ToString().PadRight(1));
+            for (int i = 0; i < compasso; i++)
+            {
+                Thread.Sleep(500);
+                System.Console.Beep(frequencia, duracao);
+            }
+        }
+
+        #region Structure
+        protected struct Note
+        {
+            Tone toneVal;
+            Duration durVal;
+
+            public Note(Tone frequency, Duration time)
+            {
+                toneVal = frequency;
+                durVal = time;
+            }
+            public Tone NoteTone { get { return toneVal; } }
+            public Duration NoteDuration { get { return durVal; } }
+        }
+        #endregion
+
+        #region Enum
+        protected enum Tone
+        {
+            REST = 0,
+            GbelowC = 196,
+            A = 220,
+            A2 = A*2,
+            Asharp = 233,
+            B = 247,
+            B2 = B*2,
+            C = 262,
+            C2 = C*2,
+            Csharp = 277,
+            D = 294,
+            Dsharp = 311,
+            E = 330,
+            F = 349,
+            Fsharp = 370,
+            G = 392,
+            Gsharp = 415,
+        }
+        protected enum Duration
+        {
+            WHOLE = 1600,
+            HALF = WHOLE / 2,
+            QUARTER = HALF / 2,
+            EIGHTH = QUARTER / 2,
+            SIXTEENTH = EIGHTH / 2,
+        }
+        #endregion
+
+        public class ConfiguracaoMetronomo
+        {
+            public int TipoCompasso { get; set; }
+            public int Velocidade { get; set; }
+        }
+
+    }
+    
+}
---
+++ XPMetronome/XPMetronome.Domain/Compasso.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XPMetronome.Domain
+{
+    public class Compasso
+    {
+        public Tipo Tipo { get; set; }
+
+        public int Batidas { get; set; }
+
+        public int Figura { get; set; }
+
+        public Compasso(int _tipo)
+        {
+            switch (_tipo)
+            {
+                case 24: Tipo = Tipo.DoisPorQuatro;
+                    break;
+                case 34: Tipo = Tipo.TreisPorQuatro;
+                    break;
+                case 44: Tipo = Tipo.QuatroPorQuatro;
+                    break;
+                case 68: Tipo = Tipo.SeisPorOito;
+                    break;
+                case 74: Tipo = Tipo.SetePorQuatro;
+                    break;
+                case 78: Tipo = Tipo.SetePorOito;
+                    break;
+            }
+        }
+    }
+
+    public enum Tipo
+    {
+        DoisPorQuatro = 24,
+        TreisPorQuatro = 34,
+        QuatroPorQuatro = 44,
+        SetePorQuatro = 74,
+        SeisPorOito = 68,
+        SetePorOito = 78
+    }
+}
