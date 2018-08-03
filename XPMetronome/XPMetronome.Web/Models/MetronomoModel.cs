0dd2dd708d5f8e30b8b1c05cc0d2f03438475119
AderSantos <adersantos@yahoo.com.br>
2018-08-03 16:50:14 -0300

    Controll implementada

---
+++ XPMetronome/XPMetronome.Web/Models/MetronomoModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace XPMetronome.Web.Models
+{
+    public class MetronomoModel
+    {
+        public string TipoCompasso { get; set; }
+
+        public int Bpm { get; set; }
+
+        [Required(ErrorMessage ="Digite quantos compassos deseja repetir.")]
+        public int QtdCompassosRepetir { get; set; }
+
+        [Required(ErrorMessage ="Digite quantos compassos deseja aumentar a velocidade.")]
+        public int QtdCompassosEvoluir { get; set; }
+
+        [Required(ErrorMessage ="Digite quantidade de bpm a ser acelerado.")]
+        public int IncrementoBpm { get; set; }
+    }
+}
